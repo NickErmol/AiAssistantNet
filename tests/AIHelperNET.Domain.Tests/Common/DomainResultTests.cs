fd8c4985960db53e0c7f471011899353b09fde39
Nickerm <70840188+NickErmol@users.noreply.github.com>
2026-06-02 17:26:21 +0200

    feat(domain): add DomainResult<T> value type
    Implements non-generic DomainResult and generic DomainResult<T> as
    readonly structs with Ok/Fail factory methods; adds 4 passing unit tests
    and suppresses doc/naming analyzer rules in the test project.

---
+++ tests/AIHelperNET.Domain.Tests/Common/DomainResultTests.cs
@@ -0,0 +1,41 @@
+using AIHelperNET.Domain.Common;
+using FluentAssertions;
+using Xunit;
+
+namespace AIHelperNET.Domain.Tests.Common;
+
+public class DomainResultTests
+{
+    [Fact]
+    public void Ok_IsSuccess_True()
+    {
+        var r = DomainResult.Ok();
+        r.IsSuccess.Should().BeTrue();
+        r.IsFailed.Should().BeFalse();
+    }
+
+    [Fact]
+    public void Fail_IsSuccess_False()
+    {
+        var r = DomainResult.Fail("bad");
+        r.IsSuccess.Should().BeFalse();
+        r.IsFailed.Should().BeTrue();
+        r.Error.Should().Be("bad");
+    }
+
+    [Fact]
+    public void OkT_CarriesValue()
+    {
+        var r = DomainResult.Ok(42);
+        r.IsSuccess.Should().BeTrue();
+        r.Value.Should().Be(42);
+    }
+
+    [Fact]
+    public void FailT_HasError()
+    {
+        var r = DomainResult.Fail<int>("oops");
+        r.IsFailed.Should().BeTrue();
+        r.Error.Should().Be("oops");
+    }
+}
